b8acd8b61eca25c49962906a46f410e8b6cc76af
TonyWang98 <wqtls1998@gmail.com>
2019-05-13 15:15:40 -0400

    Add title scene

---
+++ The-Writer-Is-Anxious/Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject titleCanvas;
+    public GameObject saveLoadCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+
+    }
+    public void LoadButton()
+    {
+        titleCanvas.SetActive(false);
+        saveLoadCanvas.SetActive(true);
+    }
+
+}
