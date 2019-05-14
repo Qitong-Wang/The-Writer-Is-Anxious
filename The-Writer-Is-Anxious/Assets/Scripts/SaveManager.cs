1eac39499a0d88146e67d22402e07fc30233a861
TonyWang98 <wqtls1998@gmail.com>
2019-05-14 14:27:39 -0400

    Add Save Manager

---
+++ The-Writer-Is-Anxious/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance { get; set; }
+    public SaveData saveData;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        Load();
+        Debug.Log(SaveDataHelper.Serialize<SaveData>(saveData));
+
+    }
+
+    public void Save()
+    {
+       PlayerPrefs.SetString("save"+1.ToString(),SaveDataHelper.Serialize<SaveData>(saveData));
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("save" + 1.ToString()))
+        {
+            saveData = SaveDataHelper.Deserialize<SaveData>(PlayerPrefs.GetString("save" + 1.ToString()));
+        }
+        else
+        {
+            saveData = new SaveData();
+            Save();
+        }
+    }
+
+}
