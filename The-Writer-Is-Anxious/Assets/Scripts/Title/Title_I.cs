6de218984e6e34c8cd5a0a6f7d53d4306610f24d
TonyWang98 <wqtls1998@gmail.com>
2019-05-15 13:48:57 -0400

    Add title animation

---
+++ The-Writer-Is-Anxious/Assets/Scripts/Title/Title_I.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Title_I : MonoBehaviour, IPointerClickHandler
+{
+   
+    public Animator title_I;
+    public GameObject buttons;
+    bool clicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clicked == false)
+        {
+            title_I.SetTrigger("Title_I_Trigger");
+            buttons.SetActive(true);
+            clicked = true;
+        }
+        
+    }
+   
+}
