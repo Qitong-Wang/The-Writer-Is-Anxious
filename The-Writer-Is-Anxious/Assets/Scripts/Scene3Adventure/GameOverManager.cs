87f6403d23c16dcda73e1d38774424717f063806
TonyWang98 <wqtls1998@gmail.com>
2019-07-28 16:00:34 -0400

    Fix bug of eventSystem

--- The-Writer-Is-Anxious/Assets/Scripts/Scene3Adventure/GameOverManager.cs
+++ The-Writer-Is-Anxious/Assets/Scripts/Scene3Adventure/GameOverManager.cs
@@ -24,6 +24,7 @@ public class GameOverManager : MonoBehaviour
     public void RestartScene()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("restartSceneName");
+        print("here");
+        SceneManager.LoadScene(restartSceneName);
     }
 }
