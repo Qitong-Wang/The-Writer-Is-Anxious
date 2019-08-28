5a2954ef9d3850bb89c669cd943895c43c47dc0d
TonyWang98 <wqtls1998@gmail.com>
2019-08-27 21:40:05 -0400

    Update phase change of dragon boss

--- The-Writer-Is-Anxious/Assets/Scripts/Scene3Adventure/Dragon.cs
+++ The-Writer-Is-Anxious/Assets/Scripts/Scene3Adventure/Dragon.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class Dragon : MonoBehaviour
 {
     public int hp;
+    int phaseNumber = 1;
+    /// <summary>
+    /// qianyao
+    /// </summary>
+    public float fireCDBefore;
+    /// <summary>
+    /// houyao
+    /// </summary>
+    public float fireCDAfter;
+    /// <summary>
+    /// player is able to click the dragon and go into the phase
+    /// </summary>
+    bool ableClick = true;
     public bool showWeakness = false;
     public GameObject weaknessObj;
     public GameObject dragonFirePrefab;
@@ -15,7 +28,7 @@ public class Dragon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("Fire");
+
     }
 
     // Update is called once per frame
@@ -23,61 +36,86 @@ public class Dragon : MonoBehaviour
     {
         
     }
+    private void OnMouseUp()
+    {
+        if (ableClick == true)
+        {
+            if(phaseNumber == 1)
+            {
+                StartCoroutine(Phase1());
+            }
+            else if (phaseNumber == 2)
+            {
+                StartCoroutine(Phase2());
+            }
+            else
+            {
+                StartCoroutine(Phase3());
+            }
+            phaseNumber++;
+        }
+    }
     /// <summary>
     /// Fire the ball to the certain aim
     /// </summary>
     /// <param name="firePosition">[0] is x direction. [1] is y direction</param>
-    void FireLavaBall(Vector2 firePosition)
+    void FireLavaBall()
     {
         GameObject fireBall;
+        int randomPosition = Random.Range(1, 4);
+        Vector2 firePosition;
+        if (randomPosition == 1)
+        {
+            firePosition = fire1Position;
+        }
+        else if (randomPosition == 2)
+        {
+            firePosition = fire2Position;
+        }
+        else
+        {
+            firePosition = fire3Position;
+        }
         fireBall = Instantiate(dragonFirePrefab, transform.position, Quaternion.identity);
         fireBall.GetComponent<DragonFire>().dragon = gameObject.GetComponent<Dragon>();
         fireBall.GetComponent<Rigidbody2D>().velocity = new Vector2(firePosition[0], firePosition[1]);
 
     }
-   
-    IEnumerator Fire()
+    IEnumerator Phase1()
     {
-        while (hp>0)
-        {
-            showWeakness = true;
-            if (hp > 0)
-            {
-                FireLavaBall(fire2Position);
-            }
-
-            yield return new WaitForSeconds(fireCD);
-            if (hp > 0)
-            {
-                FireLavaBall(fire1Position);
-                FireLavaBall(fire3Position);
-            }
-
-            yield return new WaitForSeconds(fireCD);
-            if (hp > 0)
-            {
-                FireLavaBall(fire1Position);
-                FireLavaBall(fire2Position);
-                FireLavaBall(fire3Position);
-            }
-
-            yield return new WaitForSeconds(fireCD);
-            if (showWeakness == true)
-            {
-                weaknessObj.SetActive(true);
-                yield return new WaitForSeconds(10f);
-                weaknessObj.SetActive(false);
-            }
-            else
-            {
-                yield return new WaitForSeconds(fireCD);
-            }
-           
-        }
-       
-        
+        ableClick = false;
+        yield return new WaitForSeconds(fireCDBefore);
+        FireLavaBall();
+        yield return new WaitForSeconds(fireCDAfter);
+        ableClick = true;
+        yield return null;
     }
-
-
+    IEnumerator Phase2()
+    {
+        ableClick = false;
+        yield return new WaitForSeconds(fireCDBefore);
+        FireLavaBall();
+        yield return new WaitForSeconds(fireCDBefore);
+        FireLavaBall();
+        yield return new WaitForSeconds(fireCDAfter);
+        ableClick = true;
+        yield return null;
+    }
+    IEnumerator Phase3()
+    {
+        ableClick = false;
+        yield return new WaitForSeconds(fireCDBefore);
+        weaknessObj.SetActive(true);
+        FireLavaBall();
+        yield return new WaitForSeconds(fireCDBefore);
+        FireLavaBall();
+        yield return new WaitForSeconds(fireCDBefore);
+        FireLavaBall();
+        yield return new WaitForSeconds(fireCDAfter);
+        weaknessObj.SetActive(false);
+        ableClick = true;
+        yield return null;
+    }
+ 
 
 }
