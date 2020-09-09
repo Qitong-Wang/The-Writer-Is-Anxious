885c08fc16a9f0a6b5f342985caaf1defe9875d0
Francys Dong <francysdong@gmail.com>
2020-09-09 17:45:29 -0400

    catroom

--- The-Writer-Is-Anxious/Assets/Scripts/Interactable.cs
+++ The-Writer-Is-Anxious/Assets/Scripts/Interactable.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class CatChoices
+{
+    public string intro;
+    public string order = "text"; //text:显示字 death:死 door:开门方法
+    [TextArea]
+    public string newText;
+}
+
 public class Interactable : MonoBehaviour
 {
-
+    [TextArea]
     public string intro;
     public bool normal = false;
     public bool book = false;
     public bool window = false;
+
+    //CATLEVEL
+    public bool left = false;
+    public bool right = false;
+
+    public Sprite originalImage;
+    public Sprite changedImage;
+    public bool hasChoice = false;
+    public List<CatChoices> choices;
+
 
     // Start is called before the first frame update
     void Start()
--- The-Writer-Is-Anxious/Assets/Scripts/SceneSecond.cs
+++ The-Writer-Is-Anxious/Assets/Scripts/SceneSecond.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneSecond : GlobalManager
 {
 
     public GameObject frames;
 
+    public GameObject myRoom;
+    public GameObject arrows;
+
+    public GameObject choicePanel;
+
+    public GameObject currentObject;
+    
+    private List<CatChoices> currentChoices;
+    public int room;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SceneStart());
+        room = 1;
     }
 
     public override void StateCheck()
@@ -31,6 +43,10 @@ public class SceneSecond : GlobalManager
         {
             StartCoroutine(TextGone());
         }
+        else if (state == 5)
+        {
+            StartCoroutine(InspectRoom());
+        }
     }
 
     IEnumerator Next()
@@ -41,10 +57,13 @@ public class SceneSecond : GlobalManager
 
     IEnumerator SceneStart()
     {
+        
         yield return new WaitForSeconds(0.5f);
         fadeBlack.GetComponent<Animator>().SetTrigger("In");
         yield return new WaitForSeconds(1f);
         fadeWhite.GetComponent<Animator>().SetTrigger("In");
+        myRoom.SetActive(true);
+        arrows.SetActive(true);
         frames.GetComponent<Animator>().SetTrigger("In");
         yield return new WaitForSeconds(1f);
         StartCoroutine(tm.ShowTextCat());
@@ -58,5 +77,98 @@ public class SceneSecond : GlobalManager
         fadeBlack.GetComponent<Animator>().SetTrigger("Out");
         yield return new WaitForSeconds(1.5f);
         fadeWhite.GetComponent<Animator>().SetTrigger("Out");
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(tm.ShowText());
+    }
+
+    IEnumerator InspectRoom()
+    {
+        yield return new WaitForSeconds(0.5f);
+        TapInspect();
+    }
+
+    public override void Inspect(Interactable i)
+    {
+        if (currentObject)
+        {
+            currentObject.GetComponent<SpriteRenderer>().sprite = currentObject.GetComponent<Interactable>().originalImage;
+        }
+        if (i.left)
+        {
+            room -= 1;
+            if(room < 0)
+            {
+                room = 0;
+            }
+            tm.textBoxes[0].GetComponent<Text>().text = "";
+            choicePanel.SetActive(false);
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>().SetTrigger("Left");
+            return;
+        } else if (i.right)
+        {
+            room += 1;
+            if (room >2)
+            {
+                room = 2;
+            }
+            tm.textBoxes[0].GetComponent<Text>().text = "";
+            choicePanel.SetActive(false);
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>().SetTrigger("Right");
+            return;
+        }
+        currentObject = i.gameObject;
+        tm.textBoxes[0].GetComponent<Text>().text = i.intro;
+        if (i.changedImage)
+        {
+            currentObject.GetComponent<SpriteRenderer>().sprite = currentObject.GetComponent<Interactable>().changedImage;
+        }
+        if (i.hasChoice)
+        {
+            choicePanel.SetActive(true);
+            choicePanel.transform.GetChild(0).gameObject.GetComponentInChildren<Text>().text = i.choices[0].intro;
+            choicePanel.transform.GetChild(1).gameObject.GetComponentInChildren<Text>().text = i.choices[1].intro;
+            currentChoices = new List<CatChoices> {i.choices[0], i.choices[1] };
+        } else
+        {
+            choicePanel.SetActive(false);
+        }
+    }
+
+    public void ChoiceA()
+    {
+        choicePanel.SetActive(false);
+        if (currentChoices[0].order == "text")
+        {
+            tm.textBoxes[0].GetComponent<Text>().text = currentChoices[0].newText;
+            return;
+        } else if (currentChoices[0].order == "death")
+        {
+            rayInspect = false;
+            //Death Coroutine
+        } else if (currentChoices[0].order == "door")
+        {
+            rayInspect = false;
+            //door Coroutine
+        }
+    }
+
+    public void ChoiceB()
+    {
+        choicePanel.SetActive(false);
+        if (currentChoices[1].order == "text")
+        {
+            tm.textBoxes[0].GetComponent<Text>().text = currentChoices[1].newText;
+            return;
+        }
+        else if (currentChoices[1].order == "death")
+        {
+            rayInspect = false;
+            //Death Coroutine
+        }
+        else if (currentChoices[1].order == "door")
+        {
+            rayInspect = false;
+            //door Coroutine
+        }
     }
 }
